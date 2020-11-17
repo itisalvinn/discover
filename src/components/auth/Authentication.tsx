b744eae258203f8c68202d4ea916838c76de4fda
itisalvinn <alvintfng@gmail.com>
2020-11-16 21:56:16 -0500

    updated button and login layout

--- src/components/auth/Authentication.tsx
+++ src/components/auth/Authentication.tsx
@@ -1,10 +1,29 @@
 import React, {useState} from 'react'
+import {makeStyles, Button, Container} from "@material-ui/core";
+import {Grid} from '../grid/Grid';
 
+const useStyles = makeStyles ({
+    base : {
+        backgroundColor: '#282c34',
+        minHeight: '100vh',
+        width: '100%',
+        position: 'fixed',
+        alignItems: 'normal',
+        margin: 0,
+    },
+    content : {
+        width: '50%',
+        float: 'left',
+        paddingLeft: '50px',
+        paddingRight: '40px',
+        alignItems: 'flex-start',
+    }
+});
+
+// spotify auth variables
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const redirectUri =  process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
-
 const scopes = [
   'user-read-private',
   'streaming',
@@ -12,7 +31,7 @@ const scopes = [
   'user-read-email',
 ];
 
-// get hash of url
+// get spotify hash
 const getHash = window.location.hash
       .substring(1)
       .split("&")
@@ -28,13 +47,13 @@ window.location.hash = "";
 
 // set token
 let _token = getHash.access_token;
-let _expires = getHash.expires_in;
-
 
 export const Authentication = () =>{
 
-    let [responseData, setResponseData] = useState('');
+    const [responseData, setResponseData] = useState('');
+    const styles = useStyles();
 
+    // remove later -- test method to get base spotify profile info
     const getUserProfile = (token: string) => {
         fetch('https://api.spotify.com/v1/me',{
             method: 'GET',
@@ -42,9 +61,7 @@ export const Authentication = () =>{
         })
         .then(resp => resp.json())
             .then((data) =>{
-                console.log(data);
                 setResponseData(data.display_name);
-    
         })
         .catch((err) => {
             console.log(err)
@@ -53,13 +70,38 @@ export const Authentication = () =>{
 
     return(
         <div>
-            <header className="App-header">
-                {!_token && (<a className="btnLogin" href = {`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`}>Login to Spotify</a>)}
-            </header>
-            <a>{`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}`}</a>
-            <a>{`this is the token : ${_token} blah blah blah blah123 123 123 123 expires in ${_expires} `}</a>
-            <button onClick={() => getUserProfile(_token)}> Check profile stuff thing</button>
-            <text> {`the name is \n 123 ${responseData}`} </text>
+            {/* before login */}
+            {!_token && (<div className={styles.base}> 
+                <Container className={styles.content}>
+                    <header>
+                        Mood
+                    </header>
+                    <header>
+                        music for the feels
+                    </header>
+                    <Button 
+                        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`}
+                        variant="contained"
+                        className="btnLogin"
+                        size="large">
+                        Login to Spotify
+                    </Button>
+                </Container>
+ 
+            </div>)}
+
+            {/* after login */}
+            {_token && (<div>
+                <text> {`the username is \n 123 ${responseData}`} </text>
+                <Grid/>
+                <Button 
+                    onClick={() => getUserProfile(_token)}
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                > Check profile stuff thing in logs
+                </Button>
+            </div>)}
         </div>
     )
 }
