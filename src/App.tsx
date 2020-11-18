149ca85b6ee1a0f6f5a4c0a0483c1491a987ba17
itisalvinn <alvintfng@gmail.com>
2020-11-17 22:05:12 -0500

    add general context and headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import './App.css';
-import {Authentication} from './components/auth/Authentication';
+import React, {useState} from 'react'
+import './App.css'
+import {Login} from './components/auth/LoginPage'
+import {PlayerContext} from './Context'
 
 // add login page + redirect to main
 // add different moods -- create playlists based on selected mood
+// add some footer thingy later
+// TODO: fix context provider to take in set state thingy
 
 function App() {
 
   return (
-    <div>
-      <Authentication/>
-    </div>
+    <PlayerContext.Provider value={''}>
+      <div>
+        <Login/>
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,32 +1,37 @@
 import React, {Component} from 'react'
 import StackGrid, {transitions} from 'react-stack-grid'
+import {makeStyles} from '@material-ui/core'
 import sizeMe from 'react-sizeme'
 
-const {scaleDown} = transitions
+const useStyles = makeStyles ({
+
+})
 
 // can create const items array and map them to a title/img then display them in div card
+// maybe use material ui card for each item thing
 
 export const Grid = () =>{
+
+    const styles = useStyles(); 
 
     return(
         <div>
             <StackGrid
-                columnWidth="20%"
+                columnWidth="25%"
             >
+                {/* Happy, Sad, Motivated, Mellow, Peace */}
                 <div key="key1">Item 1</div>
                 <div key="key2">Item 2</div>
                 <div key="key3">Item 3</div>
                 <div key="key4">Item 4</div>
-                <div key="key5">Item 5</div>
             </StackGrid>
             <StackGrid
-                columnWidth="20%"
+                columnWidth="25%"
             >
                 <div key="key1">Item 1</div>
                 <div key="key2">Item 2</div>
                 <div key="key3">Item 3</div>
                 <div key="key4">Item 4</div>
-                <div key="key5">Item 5</div>
             </StackGrid>
         </div>
 
